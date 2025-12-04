0a282d0a0a20b56864e8e0aba5b6838e71b47e37
Jacques Hovine <jacques@Jacquess-MacBook-Air.local>
2025-12-04 21:50:06 +1000

    display countries

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    $countries = json_decode(file_get_contents(resource_path('js/data/countries.json')), true);
+    
+    return Inertia::render('Home', [
+        'countries' => $countries
+    ]);
 })->name('home');
