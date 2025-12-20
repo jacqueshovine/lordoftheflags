0333ca87951d8023a593efed698880ea63afd85b
Jacques Hovine <jacques@Jacquess-MacBook-Air.local>
2025-12-20 17:45:06 +1000

    add separated scores foreach region

---
+++ resources/js/components/ButtonWithScore.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+defineProps({
+    maxScore: Number,
+    maxPossibleScore: Number,
+});
+</script>
+
+<template>
+    <button class="btn btn-primary">
+        <slot></slot>
+        <span class="badge badge-secondary ml-2">{{ maxScore }} / {{ maxPossibleScore }}</span>
+    </button>
+</template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,30 +1,62 @@
 <script setup>
 import { computed, ref } from 'vue';
 import { shuffle } from '../utils';
+
 import FlagGrid from '../components/FlagGrid.vue';
 import ScoreDisplay from '../components/ScoreDisplay.vue';
 import GameQuestion from '../components/GameQuestion.vue';
+import ButtonWithScore from '../components/ButtonWithScore.vue';
 
 const props = defineProps({
   countries: Array
 });
 
-// const countries = ref(props.countries || []);
 const countries = computed(() => {
+    if (currentFilter.value) {
+        return props.countries.filter(country => country.continent === currentFilter.value);
+    }
     return props.countries || [];
 });
 
 const gameRunning = ref(false);
 const roundsLeft = ref(0);
+const currentFilter = ref(null);
 
 const currentScore = ref(0);
-const maxScore = ref(0);
+const maxAchievedScores = ref({
+    World: 0,
+    Africa: 0,
+    America: 0,
+    Asia: 0,
+    Europe: 0,
+    Oceania: 0
+});
+const regions = ['World', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+const regionScores = computed(() => {
+    const scores = {};
+    
+    regions.forEach(region => {
+        const maxPossible = region === 'World' 
+            ? props.countries?.length || 0
+            : props.countries?.filter(c => c.continent === region).length || 0;
+        
+        scores[region] = {
+            maxPossible,
+            maxAchieved: maxAchievedScores.value[region]
+        };
+    });
+    
+    return scores;
+});
 
 const countryToGuess = ref('');
 const countryPossibilities = ref([]);
 const selectedAnswer = ref(null);
 
-function initGame() {
+function initGame(filter = null) {
+    // Filter countries based on the selected region if provided
+    currentFilter.value = filter;
+
     // Shuffle the countries array to randomize the order every time
     countries.value = shuffle(countries.value);
     gameRunning.value = true;
@@ -69,9 +101,13 @@ function checkAnswer(selectedCountry) {
 
 function endGame() {
     gameRunning.value = false;
-    if (currentScore.value > maxScore.value) {
-        maxScore.value = currentScore.value;
+    
+    // Update the max score for the current region
+    const region = currentFilter.value || 'World';
+    if (currentScore.value > maxAchievedScores.value[region]) {
+        maxAchievedScores.value[region] = currentScore.value;
     }
+    
     currentScore.value = 0;
 }
 
@@ -82,12 +118,19 @@ function endGame() {
         <h1>Welcome</h1>
         <p>This is the main landing page of the application.</p>
 
-        <button v-show="!gameRunning" @click="initGame()">Start Game</button>
+        <div v-for="region in regions" :key="region">
+            <ButtonWithScore
+                v-if="!gameRunning"
+                :max-score="regionScores[region].maxAchieved"
+                :max-possible-score="regionScores[region].maxPossible"
+                @click="initGame(region === 'World' ? null : region)"
+            >{{ region }}</ButtonWithScore>
+        </div>
 
         <ScoreDisplay 
-            v-show="gameRunning"
+            v-if="gameRunning"
             :current-score="currentScore"
-            :max-score="maxScore"
+            :max-score="regionScores[currentFilter || 'World'].maxAchieved"
         />
 
         <div v-if="gameRunning">
